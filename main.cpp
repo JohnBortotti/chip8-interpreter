314a0887e9b0170a3a567f136761b8cbd9c2d160
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-11-03 18:21:10 -0300

    Add fontset and load_game()
    Add fontset data and load_fontset()
    Add load_game()

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <SDL2/SDL.h>
 
 class chip8
@@ -21,10 +22,33 @@ public:
     // graphics matrix (64x32 pixels / bits)
     bool gfx[64][32];
 
+    bool draw_flag;
+
+    // preloaded fontset
+    unsigned char fontset[80] =
+        {
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+
     // delay timer
     unsigned short delay_timer;
 
-    // sound timer 
+    // sound timer
     unsigned short sound_timer;
 
     // stack and stack_pointer
@@ -32,21 +56,21 @@ public:
     unsigned short stack_pointer;
 
     unsigned char keypad[16];
-
     void initialize()
     {
         PC = 0x200;
         opcode = 0;
-        I = 0;
+        I = 0x00;
+        draw_flag = 0;
 
         clear_gfx();
         clear_memory();
         clear_registers();
         clear_stack();
-        // [ ] validate clear functions
-        // [ ] load fonts
+        reset_timers();
+        load_fontset();
 
-        // [ ] load game
+        load_game("IBM_Logo.ch8");
     }
 
     void emulate_cycle()
@@ -95,12 +119,26 @@ private:
         }
     }
 
+    // load fontset values to memory
+    void load_fontset()
+    {
+        for (int x = 0; x <= 80; x++)
+        {
+            memory[x] = fontset[x];
+        }
+    }
+
     void clear_memory()
     {
         for (unsigned char &x : memory)
         {
             x = 0;
         }
+    }
+
+    void reset_timers()
+    {
+        delay_timer, sound_timer = 0;
     }
 
     void clear_registers()
@@ -120,32 +158,58 @@ private:
 
         stack_pointer = 0;
     }
+
+    // open file, extract its bits and load into memory
+    void load_game(std::string path)
+    {
+        std::ifstream f(path, std::ios::binary | std::ios::in);
+
+        if (!f.is_open())
+        {
+            throw std::invalid_argument("invalid ROM path");
+        }
+
+        // holder char
+        char c;
+        // start on memory address
+        int adrs = 0x200;
+
+        for (int i = 0x200; f.get(c); i++)
+        {
+            if (adrs >= 4096)
+            {
+                throw std::invalid_argument("file size too big");
+            }
+            memory[i] = (uint8_t)c;
+            adrs++;
+        }
+    }
 };
 
 int main()
 {
     chip8 chip8;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
-    SDL_Surface *screenSurface;
+    // SDL_Window *window;
+    // SDL_Renderer *renderer;
+    // SDL_Surface *screenSurface;
 
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK);
-    SDL_CreateWindowAndRenderer(900, 600, 0, &window, &renderer);
-    SDL_RenderClear(renderer);
+    // SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK);
+    // SDL_CreateWindowAndRenderer(900, 600, 0, &window, &renderer);
+    // SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-    SDL_Rect rect;
-    rect.x = 0;
-    rect.y = 0;
-    rect.w = 12;
-    rect.h = 12;
+    // SDL_Rect rect;
+    // rect.x = 0;
+    // rect.y = 0;
+    // rect.w = 12;
+    // rect.h = 12;
 
     chip8.initialize();
-    chip8.gfx[10][20] = 1;
-    chip8.render_SDL_from_gfx(&renderer, rect);
+    // chip8.gfx[10][20] = 1;
+    // chip8.render_SDL_from_gfx(&renderer, rect);
 
-    SDL_Delay(3000);
+    // SDL_Delay(3000);
 
     // SDL_DestroyRenderer(renderer);
     // SDL_DestroyWindow(window);
