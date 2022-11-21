e112bb4e59eab70e5f9f781269121c9c41f0ed6d
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-11-21 20:50:14 -0300

    Add first tests and link chip8 with SDL
    Add first opcode unit test
    Link Chip8 with SDL lib

--- tests/opcode_tests.cpp
+++ tests/opcode_tests.cpp
@@ -2,4 +2,18 @@
 #include "../chip8.h"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("opcode xpto") { REQUIRE(1 == 1); }
+chip8 chip8;
+
+TEST_CASE("opcode 0x00E0 should clear gfx bitmap") {
+  chip8.PC = 0;
+  chip8.gfx[1][2] = 1;
+  chip8.gfx[14][3] = 1;
+
+  chip8.memory[0] = 0x00;
+  chip8.memory[1] = 0xE0;
+
+  chip8.emulate_cycle();
+
+  REQUIRE(chip8.gfx[1][2] == 0);
+  REQUIRE(chip8.gfx[14][3] == 0);
+}
