e2e0613fe156ac7c587d8ef1c2ae6a2bf91637de
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-12-10 17:08:22 -0300

    Starting keypad implementation
    Create chip8 keypad handling to play

--- main.cpp
+++ main.cpp
@@ -1,6 +1,92 @@
 #include "./chip8.h"
 #include <SDL2/SDL.h>
 
+void handle_inputs(SDL_Event event, chip8 chip8) {
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+    case SDL_QUIT: {
+      exit(0);
+      break;
+    }
+    case SDL_KEYDOWN: {
+      switch (event.key.keysym.sym) {
+      case SDLK_1: {
+        chip8.keypad[0x1] = 1;
+        break;
+      }
+      case SDLK_2: {
+        chip8.keypad[0x2] = 1;
+        break;
+      }
+      case SDLK_3: {
+        chip8.keypad[0x3] = 1;
+        break;
+      }
+      case SDLK_4: {
+        chip8.keypad[0xc] = 1;
+        break;
+      }
+      case SDLK_q: {
+        chip8.keypad[0x4] = 1;
+        break;
+      }
+      case SDLK_w: {
+        chip8.keypad[0x5] = 1;
+        break;
+      }
+      case SDLK_e: {
+        chip8.keypad[0x6] = 1;
+        break;
+      }
+      case SDLK_r: {
+        chip8.keypad[0xd] = 1;
+        break;
+      }
+      case SDLK_a: {
+        chip8.keypad[0x7] = 1;
+        break;
+      }
+      case SDLK_s: {
+        chip8.keypad[0x8] = 1;
+        break;
+      }
+      case SDLK_d: {
+        chip8.keypad[0x9] = 1;
+        break;
+      }
+      case SDLK_f: {
+        chip8.keypad[0xe] = 1;
+        break;
+      }
+      case SDLK_z: {
+        chip8.keypad[0xa] = 1;
+        break;
+      }
+      case SDLK_x: {
+        chip8.keypad[0x0] = 1;
+        break;
+      }
+      case SDLK_c: {
+        chip8.keypad[0xb] = 1;
+        break;
+      }
+      case SDLK_v: {
+        chip8.keypad[0xf] = 1;
+        break;
+      }
+      default: {
+        break;
+      }
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+    }
+  }
+}
+
 void play_mode(std::string rom_path) {
   chip8 chip8;
   chip8.initialize(rom_path);
@@ -8,13 +94,8 @@ void play_mode(std::string rom_path) {
   while (true) {
     SDL_Event event;
 
-    while (SDL_PollEvent(&event)) {
-      if (event.type == SDL_QUIT) {
-        exit(0);
-      }
-
-      chip8.emulate_cycle();
-    }
+    handle_inputs(event, chip8);
+    chip8.emulate_cycle();
   }
 }
 
